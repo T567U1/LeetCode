b1858a05b90fdc35da1bcb066352ea150e950f20
T567U1 <f-dominguez1@outlook.com>
2020-10-05 21:41:43 -0800

    1009?

--- Easy/1009. Complement of Base 10 Integer.py
+++ Easy/1009. Complement of Base 10 Integer.py
@@ -8,3 +8,7 @@ class Solution:
                 rnum += '1'
 
         return int(rnum, 2)
+
+#get the lenght substract 2 the b1 characters shift to left = 2 ^ len(c)
+#and substract original number math hack....
+#return (1<<(len(bin(N)) - 2)) - 1 - N
